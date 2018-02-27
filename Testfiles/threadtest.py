d1dfe672537875677d9fd1cb5eefd464c33fcc60
Draco-314 <jeremeyer@gmail.com>
2018-02-27 14:29:16 -0500

    Added example multithreading python script

---
+++ Testfiles/threadtest.py
@@ -0,0 +1,17 @@
+import threading
+import time
+
+ans = -1
+
+def wait_in():
+  while(ans < 0):
+    print("nut")
+    time.sleep(5)
+  print("hey bud")
+
+th1 = threading.Thread(None, wait_in, "Thread-1")
+th1.start()
+
+print(th1.is_alive())
+
+ans = int(input("I should be blocking the program, but until you enter an int the alternate thread is still running\n"))
